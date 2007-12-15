87a82adffc114ed58e045413c330a251cad29c07
Gustavo J. A. M. Carneiro <gjc@src.gnome.org>
2007-12-15 16:05:22 +0000

    avoid error hiding
    svn path=/trunk/; revision=446

--- mediaprofiles/mediaprofilesmodule.c
+++ mediaprofiles/mediaprofilesmodule.c
@@ -16,17 +16,21 @@ initmediaprofiles (void)
 {
     PyObject *m, *d;
     
-    /* TODO: We leak a ref to gconf client here, who cares.. */
     gnome_media_profiles_init (gconf_client_get_default ());
 
     m = Py_InitModule("mediaprofiles", pymediaprofiles_functions);
     d = PyModule_GetDict(m);
+    if (PyErr_Occurred())
+        return;
 
     init_pygtk();
+    if (PyErr_Occurred())
+        return;
     init_pygobject();
+    if (PyErr_Occurred())
+        return;
     
     pymediaprofiles_register_classes(d);
-
     if (PyErr_Occurred())
-        Py_FatalError("could not initialise module mediaprofiles");
+        return;
 }
